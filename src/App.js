5c0dc9860a22ae217166cad31f7d30f47272bfed
minseokKim6823 <alstjr6823@naver.com>
2025-06-10 23:57:00 +0900

    State

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import MyComponent from './MyComponent';
+import Counter from './Counter'
 
-const App = () => {
-  return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
+const App = () =>{
+  return <Counter/>
 };
-
 export default App;
